22157b68efeb94b7b64f3471c05f6f7642f200ff
Wilson Bilkovich <wilson@supremetyrant.com>
2007-08-04 23:15:23 -0800

    More tests
    [git-p4: depot-paths = "//src/vlad/dev/": change = 3373]

--- test/test_vlad.rb
+++ test/test_vlad.rb
@@ -8,6 +8,20 @@ class TestVladSet < Test::Unit::TestCase
 
   def test_set
     @vlad.set :foo, 5
+    assert_equal 5, @vlad.env[:foo]
   end
+
+  def test_set_with_block
+    x = 1
+    @vlad.set :foo { x += 2 } 
+    assert_equal 3, @vlad.env[:foo]
+    assert_equal 3, @vlad.env[:foo]
+  end 
+  
+  def test_set_with_dynamic_string
+  end 
+
+  def test_set_with_immediate_value
+  end 
 end
 
