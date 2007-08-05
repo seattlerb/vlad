ad02024675662e5e1dfbffb8986e57f6dec2f5b7
Ryan Davis <ryand@zenspider.com>
2007-08-04 22:24:37 -0800

    Implemented basic config/deploy.rb stuffs. Got set/role working in a fully namespaced way.
    [git-p4: depot-paths = "//src/vlad/dev/": change = 3370]

--- lib/vlad.rb
+++ lib/vlad.rb
@@ -1,3 +1,30 @@
+require 'singleton'
+
 class Vlad
   VERSION = '1.0.0'
+
+  include Singleton
+
+  def initialize
+    @env = {}
+    @roles = {}
+
+    instance_eval File.read("config/deploy.rb")
+  end
+
+  def set name, val = nil, &b
+    @env[name] = val || b
+  end
+
+  def role name, host, args = nil
+    @roles[name] = [host, args]
+  end
+
+  def method_missing name, *other
+    if @env.has_key? name and other.empty? then
+      @env[name]
+    else
+      super
+    end
+  end
 end
