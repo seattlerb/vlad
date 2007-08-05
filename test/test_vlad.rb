6539a778cc5481ca9b93e6d7530dde60d0727de4
Wilson Bilkovich <wilson@supremetyrant.com>
2007-08-04 23:19:06 -0800

    Fixed stupid busted parse error
    [git-p4: depot-paths = "//src/vlad/dev/": change = 3374]

--- test/test_vlad.rb
+++ test/test_vlad.rb
@@ -8,14 +8,14 @@ class TestVladSet < Test::Unit::TestCase
 
   def test_set
     @vlad.set :foo, 5
-    assert_equal 5, @vlad.env[:foo]
+    assert_equal 5, @vlad.foo
   end
 
   def test_set_with_block
     x = 1
-    @vlad.set :foo { x += 2 } 
-    assert_equal 3, @vlad.env[:foo]
-    assert_equal 3, @vlad.env[:foo]
+    @vlad.set(:foo) { x += 2 } 
+    assert_equal 3, @vlad.foo
+    assert_equal 3, @vlad.foo
   end 
   
   def test_set_with_dynamic_string
