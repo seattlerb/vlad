90f100002c92e7fb49e117fa7d293891aa26186b
Wilson Bilkovich <wilson@supremetyrant.com>
2007-08-05 16:16:32 -0800

    Replace 'foo' strings with 'test'
    [git-p4: depot-paths = "//src/vlad/dev/": change = 3401]

--- test/test_vlad.rb
+++ test/test_vlad.rb
@@ -23,33 +23,33 @@ class TestVlad < Test::Unit::TestCase
   end
 
   def test_set
-    @vlad.set :foo, 5
-    assert_equal 5, @vlad.foo
+    @vlad.set :test, 5
+    assert_equal 5, @vlad.test
   end
 
   def test_set_lazy_block_evaluation
-    @vlad.set(:foo) { fail "lose" }
-    assert_raise(RuntimeError) { @vlad.foo }
+    @vlad.set(:test) { fail "lose" }
+    assert_raise(RuntimeError) { @vlad.test }
   end
 
   def test_set_with_block
     x = 1
-    @vlad.set(:foo) { x += 2 }
+    @vlad.set(:test) { x += 2 }
 
-    assert_equal 3, @vlad.foo
-    assert_equal 3, @vlad.foo
+    assert_equal 3, @vlad.test
+    assert_equal 3, @vlad.test
   end
 
   def test_set_with_block_and_value
     e = assert_raise(ArgumentError) do
-      @vlad.set(:foo, 5) { 6 }
+      @vlad.set(:test, 5) { 6 }
     end
     assert_equal "cannot provide both a value and a block", e.message
   end
 
   def test_set_with_nil
-    @vlad.set(:foo, nil)
-    assert_equal nil, @vlad.foo
+    @vlad.set(:test, nil)
+    assert_equal nil, @vlad.test
   end
 
   def test_set_with_reserved_name
@@ -58,50 +58,50 @@ class TestVlad < Test::Unit::TestCase
   end
 
   def test_role
-    @vlad.role :app, "foo.example.com"
-    expected = {"foo.example.com" => {}}
+    @vlad.role :app, "test.example.com"
+    expected = {"test.example.com" => {}}
     assert_equal expected, @vlad.roles[:app]
   end
 
   def test_role_multiple_hosts
-    @vlad.role :app, "foo.example.com"
+    @vlad.role :app, "test.example.com"
     @vlad.role :app, "yarr.example.com", :no_release => true
     expected = {
-      "foo.example.com" => {},
+      "test.example.com" => {},
       "yarr.example.com" => {:no_release => true}
     }
     assert_equal expected, @vlad.roles[:app]
   end
 
   def test_role_multiple_roles
-    @vlad.role :app, "foo.example.com", :primary => true
+    @vlad.role :app, "test.example.com", :primary => true
     @vlad.role :db, "yarr.example.com", :no_release => true
     expected_db = { "yarr.example.com" => {:no_release => true} }
     assert_equal expected_db, @vlad.roles[:db]
-    expected_app = { "foo.example.com" => {:primary => true} }
+    expected_app = { "test.example.com" => {:primary => true} }
     assert_equal expected_app, @vlad.roles[:app]
   end
 
   def test_host
-    @vlad.host "foo.example.com", :app, :db
-    expected = {"foo.example.com" => {}}
+    @vlad.host "test.example.com", :app, :db
+    expected = {"test.example.com" => {}}
     assert_equal expected, @vlad.roles[:app]
     assert_equal expected, @vlad.roles[:db]
   end
 
   def test_host_multiple_hosts
-    @vlad.host "foo.example.com", :app, :db
+    @vlad.host "test.example.com", :app, :db
     @vlad.host "yarr.example.com", :app, :db, :no_release => true
 
     expected = {
-      "foo.example.com" => {},
+      "test.example.com" => {},
       "yarr.example.com" => {:no_release => true}
     }
 
     assert_equal expected, @vlad.roles[:app]
     assert_equal expected, @vlad.roles[:db]
-    assert_not_equal(@vlad.roles[:db]["foo.example.com"].object_id,
-                     @vlad.roles[:app]["foo.example.com"].object_id)
+    assert_not_equal(@vlad.roles[:db]["test.example.com"].object_id,
+                     @vlad.roles[:app]["test.example.com"].object_id)
   end
 
   def test_run
