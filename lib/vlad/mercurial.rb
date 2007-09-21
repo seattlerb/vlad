c73ae791a2b651d9417aa4af771564b02a180a3c
Wilson Bilkovich <wilson@supremetyrant.com>
2007-09-21 15:35:08 -0800

    Apply Mercurial SCM support patch. Closes ticket 13475
    [git-p4: depot-paths = "//src/vlad/dev/": change = 3603]

---
+++ lib/vlad/mercurial.rb
@@ -0,0 +1,32 @@
+class Vlad::Mercurial
+
+  ##
+  # Returns the command that will check out +revision+ from the repository
+  # into directory +destination+
+
+  def checkout(revision, destination)
+    revision = 'tip' if revision =~ /^head$/i
+    "hg pull -r #{revision} -R #{destination} #{repository}"
+  end
+
+  ##
+  # Returns the command that will export +revision+ from the repository into
+  # the directory +destination+.
+
+  def export(revision_or_source, destination)
+    revision_or_source = 'tip' if revision_or_source =~ /^head$/i
+    if revision_or_source =~ /^(\d+|tip)$/i then
+      "hg archive -r #{revision_or_source} -R #{repository} #{destination}"
+    else
+      "hg archive -R #{revision_or_source} #{destination}"
+    end
+  end
+
+  ##
+  # Returns a command that maps human-friendly revision identifier +revision+
+  # into a subversion revision specification.
+
+  def revision(revision)
+    "`hg identify -R #{repository} | cut -f1 -d\\ `"
+  end
+end
---
+++ test/test_vlad_mercurial.rb
@@ -0,0 +1,26 @@
+require 'test/vlad_test_case'
+require 'vlad'
+require 'vlad/mercurial'
+
+class TestVladMercurial < Test::Unit::TestCase
+  def setup
+    @scm = Vlad::Mercurial.new
+    set :repository, "http://repo/project"
+  end
+
+  def test_checkout
+    cmd = @scm.checkout 'head', '/the/place'
+    assert_equal 'hg pull -r tip -R /the/place http://repo/project', cmd
+  end
+
+  def test_export
+    cmd = @scm.export 'head', '/the/place'
+    assert_equal 'hg archive -r tip -R http://repo/project /the/place', cmd
+  end
+
+  def test_revision
+    cmd = @scm.revision('tip')
+    expected = "`hg identify -R http://repo/project | cut -f1 -d\\ `"
+    assert_equal expected, cmd
+  end
+end
