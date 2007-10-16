d484b9e41f89f0fec511e5151f4edec839883596
Ryan Davis <ryand@zenspider.com>
2007-10-16 02:24:36 -0800

    Added Garry Dolley's git patch after some cleanup. Reviewed for accuracy by Evan Phoenix.
    [git-p4: depot-paths = "//src/vlad/dev/": change = 3628]

---
+++ lib/vlad/git.rb
@@ -0,0 +1,43 @@
+class Vlad::Git
+
+  set :source, Vlad::Git.new
+  set :git_cmd, "git"
+
+  ##
+  # Returns the command that will check out +revision+ from the
+  # repository into directory +destination+.  +revision+ can be any
+  # SHA1 or equivalent (e.g. branch, tag, etc...)
+
+  def checkout(revision, destination)
+    destination = 'repo' if destination == '.'
+    revision = 'HEAD' if revision =~ /head/i
+
+    [ "rm -rf #{destination}",
+      "#{git_cmd} clone #{repository} #{destination}",
+      "cd #{destination}",
+      "#{git_cmd} checkout -f -b deployed-#{revision} #{revision}"
+    ].join(" && ")
+  end
+
+  ##
+  # Returns the command that will export +revision+ from the repository into
+  # the directory +destination+.
+
+  def export(revision, destination)
+    revision = 'HEAD' if revision == "."
+
+    [ "mkdir -p #{destination}",
+      "#{git_cmd} archive #{revision} | (cd #{destination} && tar xf -)"
+    ].join(" && ")
+  end
+
+  ##
+  # Returns a command that maps human-friendly revision identifier +revision+
+  # into a git SHA1.
+
+  def revision(revision)
+    revision = 'HEAD' if revision =~ /head/i
+
+    "`#{git_cmd} rev-parse #{revision}`"
+  end
+end
---
+++ test/test_vlad_git.rb
@@ -0,0 +1,39 @@
+require 'test/vlad_test_case'
+require 'vlad'
+require 'vlad/git'
+
+class TestVladGit < VladTestCase
+  def setup
+    super
+    @scm = Vlad::Git.new
+    set :repository, "git@myhost:/home/john/project1"
+  end
+
+  def test_checkout
+    # Checkout to the current directory (which is what the :update task passes)
+    cmd = @scm.checkout 'master', '.'
+    assert_equal 'rm -rf repo && git clone git@myhost:/home/john/project1 repo && cd repo && git checkout -f -b deployed-master master', cmd
+
+    # Mimic :update task
+    # 'head' should become HEAD
+    cmd = @scm.checkout 'head', '.'
+    assert_equal 'rm -rf repo && git clone git@myhost:/home/john/project1 repo && cd repo && git checkout -f -b deployed-HEAD HEAD', cmd
+
+    # Checkout to a relative path
+    cmd = @scm.checkout 'master', 'some/relative/path'
+    assert_equal 'rm -rf some/relative/path && git clone git@myhost:/home/john/project1 some/relative/path && cd some/relative/path && git checkout -f -b deployed-master master', cmd
+  end
+
+  def test_export
+    cmd = @scm.export 'master', 'the/release/path'
+    assert_equal 'mkdir -p the/release/path && git archive master | (cd the/release/path && tar xf -)', cmd
+  end
+
+  def test_revision
+    ['head', 'HEAD'].each do |head|
+      cmd = @scm.revision(head)
+      expected = "`git rev-parse HEAD`"
+      assert_equal expected, cmd
+    end
+  end
+end
