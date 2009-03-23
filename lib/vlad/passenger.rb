d57794c91e13c866a47540b554598c4c7cc2a6f3
Ryan Davis <ryand@zenspider.com>
2009-03-23 14:40:23 -0800

    - passenger:start_app wasn't using the latest_release path [git-p4: depot-paths = "//src/vlad/dev/": change = 4778]

--- lib/vlad/passenger.rb
+++ lib/vlad/passenger.rb
@@ -3,6 +3,6 @@ require 'vlad'
 namespace :vlad do
   desc 'Restart Passenger'
   remote_task :start_app, :roles => :app do
-    run "touch #{release_path}/tmp/restart.txt"
+    run "touch #{latest_release}/tmp/restart.txt"
   end
 end
