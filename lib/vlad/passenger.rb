745a9884425a81608cdbf4b288b124279bec3726
Ryan Davis <ryand@zenspider.com>
2011-07-18 14:01:15 -0800

    - Fixed vlad:start_app for passenger computing a new latest_release (dbloete) [git-p4: depot-paths = "//src/vlad/dev/": change = 6445]

--- lib/vlad/passenger.rb
+++ lib/vlad/passenger.rb
@@ -3,6 +3,6 @@ require 'vlad'
 namespace :vlad do
   desc 'Restart Passenger'
   remote_task :start_app, :roles => :app do
-    run "touch #{latest_release}/tmp/restart.txt"
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
