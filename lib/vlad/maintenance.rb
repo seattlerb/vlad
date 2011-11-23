36cfeb82c583d51607cc82968bbdc78703cda0a4
Ryan Davis <ryand@zenspider.com>
2011-11-22 20:37:48 -0800

    - Updated blog link. (SixArm) - Add task descriptions to maintenance tasks (SixArm) [git-p4: depot-paths = "//src/vlad/dev/": change = 6759]

--- lib/vlad/maintenance.rb
+++ lib/vlad/maintenance.rb
@@ -3,15 +3,20 @@ require 'vlad'
 ##
 # See the following documents for recipes:
 #
-# * http://clarkware.com/cgi/blosxom/2007/01/05/CustomMaintenancePages
+# * http://clarkware.com/blog/2007/1/5/custom-maintenance-pages
 # * http://blog.nodeta.fi/2009/03/11/stopping-your-rails-application-with-phusion-passenger/
 #
 
 namespace :vlad do
   namespace :maintenance do
+
+    desc "Turn on the maintenance web page"
+
     remote_task :on, :roles => [:web] do
       run "cp -f #{shared_path}/config/maintenance.html #{shared_path}/system/"
     end
+
+    desc "Turn off the maintenance web page"
 
     remote_task :off, :roles => [:web] do
       run "rm -f #{shared_path}/system/maintenance.html"
