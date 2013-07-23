9cbc77409e35da8356508b8fae2a546d4145bdf0
Ryan Davis <ryand@zenspider.com>
2013-07-23 14:12:02 -0800

    prepped for release [git-p4: depot-paths = "//src/vlad/dev/": change = 8830]

--- lib/vlad.rb
+++ lib/vlad.rb
@@ -21,7 +21,7 @@ module Vlad
 
   ##
   # This is the version of Vlad you are running.
-  VERSION = "2.6.0"
+  VERSION = "2.6.1"
 
   ##
   # Loads tasks file +tasks_file+ and various recipe styles as a hash
